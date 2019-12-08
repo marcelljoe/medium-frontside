f05abb47ddc4bd196f3e0935966ca96fc001b480
Marcellino Jonathan <marselljonathan@gmail.com>
2019-12-08 17:48:14 +0700

    1.register

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles(theme => ({
     alignItems: 'center',
     justifyContent: 'center',
     height: 'auto',
-    width: '950px',
+    width: 'auto',
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
