f35c020ae98e3f23b547f119cdb60b9cfd763817
Marcellino Jonathan <marselljonathan@gmail.com>
2019-12-08 22:47:00 +0700

    1.register

--- src/App.js
+++ src/App.js
@@ -1,123 +1,20 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import Img from 'react-image';
-import {Fade, Backdrop, Modal, makeStyles, Grid, Container, Button, Menu, TextField, Link} from '@material-ui/core';
+import {Button, Header, Icon, Modal} from 'semantic-ui-react';
 import './App.css';
-
-
-const useStyles = makeStyles(theme => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 'auto',
-    width: 'auto',
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
-
-export default function TransitionsModal() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <div>
-      <Button variant="contained" color="primary" type="button" onClick={handleOpen}>
-        Getting Started
-      </Button>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-          <Grid
-      justify="center"
-      container
-      >
-      <Grid
-      item xs={3}
-      direction="column"
-      justify="center"
-      alignItems="flex-end">
-
-        <Img src="./leftimg.png" width="214" height="550"/>
-      </Grid>
-      <Grid
-        item xs={5}
-        direction="column"
-        justify="flex-start"
-        alignItems="center">
-        
-        <h2>Join Medium.</h2>
-        <h4>Create an account to receive great stories in your inbox, personalize your homepage, and follow authors and topics that you love.</h4>
-        <TextField id="standard-basic" label="Fullname"/>
-        <br/>
-        <TextField id="standard-basic" label="Username"/>
-        <br/>
-        <TextField id="standard-basic" label="Password" type="password"/>
-        <br/><br/>
-        <Button variant="outlined" color="primary">Register</Button>
-        <br/><br/>
-        <Link href="###">Already registered? Click here to Log In.</Link>
-        </Grid>
-        <Grid
-        item xs={3}
-          direction="column"
-          justify="center"
-          alignItems="flex-start"
-          ><Img src="./rightimg.png" width="214" height="550"/>
-        </Grid>
-        </Grid>
-          </div>
-        </Fade>
-      </Modal>
-    </div>
-  );
-}
+import Registration from './Registration';
+import Login from './Login';
 
 class App extends Component {
   render(){
   return (
     <div className="App">
-      <Header />
-      {/* <Content /> */}
+      <Registration />
+      <Login />
     </div>
   );
   }
 }
-class Header extends Component {
-  render(){
-    return (
-      <div className="container">
-        <div class="pallet" id="boxzone">
-        </div>
-        <div class="subpallet"> 
-        </div>
-      </div>
-    );
-  }
-}
 
+export default App;
 
---
+++ src/Registration.js
@@ -0,0 +1,65 @@
+import _ from 'lodash';
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Form, Input, Grid, Image, Button, Header, Icon, Modal} from 'semantic-ui-react';
+import './App.css';
+
+const columns = _.times(16, (i) => (
+    <Grid.Column key={i}>
+    <Image src='/images/wireframe/image.png' />
+    </Grid.Column>
+))
+const InputExampleInput = () => <Input placeholder=""/>
+
+const GridExampleGrid = () => 
+<Grid>{columns}</Grid>
+
+class Registration extends Component{
+    render(){
+        return (
+            <Modal trigger={<Button>Register</Button>} size="medium">
+                
+                <Grid columns={3} divided verticalAlign="top">
+                    <Grid.Row>
+                    <Grid.Column>
+                        <Image floated="left" verticalAlign="top" src='https://miro.medium.com/max/214/1*MQH4A5bsyRz4AWh5V4IfvQ.png' />
+                    </Grid.Column>
+                    <Grid.Column width={5.7}>
+                    <div>
+                        <br/>
+                        <h2 align="center">Join Medium.</h2>
+                        <h4 align="center">
+                        Create an account to receive great stories in your inbox, personalize your homepage, and follow authors and topics that you love.
+                        </h4>
+                        <Form>
+                        <Form.Field align="center">
+                            <Input size="small" type="text" placeholder="Fullname" />
+                            </Form.Field>
+                        <Form.Field align="center">
+                            <Input size="small" type="text" placeholder="Username" />
+                        </Form.Field>
+                        <Form.Field align="center">
+                            <Input size="small" type="password" placeholder="Password" />
+                        </Form.Field>
+                        <Form.Field align="center">
+                            <Button color="black" size="small">Register</Button>
+                        </Form.Field>
+                        <Form.Field align="center">
+                            <h4>Already have an Account? <a href="###">Sign In</a></h4>
+                            <h4>To make Medium work, we log user data and share it with service providers. Click “Sign Up” above to accept Medium’s<a href="###">Terms of Service</a>&<a href="###">Privacy Policy</a></h4>
+                        </Form.Field>
+                        </Form>
+                    </div>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Image floated="right" verticalAlign="top" src='https://miro.medium.com/max/214/1*lhbp8cxKdkDB-MgmwIPE5w.png' />
+                    </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+                </Modal>
+
+    );
+    }
+    }
+
+    export default Registration;
