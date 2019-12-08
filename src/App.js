7b9914d0cc24081d49f7eaf68a247f2f549b2627
Marcellino Jonathan <marselljonathan@gmail.com>
2019-12-08 17:43:47 +0700

    1.register

--- src/App.js
+++ src/App.js
@@ -1,57 +1,77 @@
 import React,{Component} from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import Img from 'react-image';
-import {Grid, Container, Button, Menu, TextField, Link} from '@material-ui/core';
+import {Fade, Backdrop, Modal, makeStyles, Grid, Container, Button, Menu, TextField, Link} from '@material-ui/core';
 import './App.css';
 
 
-class App extends Component {
-  render(){
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 'auto',
+    width: '950px',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function TransitionsModal() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <Content />
-    </div>
-  );
-  }
-}
-class Header extends Component {
-  render(){
-    return (
-      <div className="container">
-        <div class="pallet" id="boxzone">
-        </div>
-        <div class="subpallet"> 
-        </div>
-      </div>
-    );
-  }
-}
-class Content extends Component {
-  render(){
-    return (
-      
-      <Grid
-      container
-      spacing={3}
+    <div>
+      <Button variant="contained" color="primary" type="button" onClick={handleOpen}>
+        Getting Started
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+          <Grid
       justify="center"
-      alignItems="center"
+      container
       >
       <Grid
       item xs={3}
       direction="column"
       justify="center"
-      alignItems="flex-end"
-      >
+      alignItems="flex-end">
+
         <Img src="./leftimg.png" width="214" height="550"/>
       </Grid>
       <Grid
-        item xs={3}
+        item xs={5}
         direction="column"
-        justify="center"
+        justify="flex-start"
         alignItems="center">
-        <h2 class="signuptitle">Join Medium.</h2>
-        <h4 class="signupsubtitle">Create an account to receive great stories in your inbox, personalize your homepage, and follow authors and topics that you love.</h4>
+        
+        <h2>Join Medium.</h2>
+        <h4>Create an account to receive great stories in your inbox, personalize your homepage, and follow authors and topics that you love.</h4>
         <TextField id="standard-basic" label="Fullname"/>
         <br/>
         <TextField id="standard-basic" label="Username"/>
@@ -70,11 +90,34 @@ class Content extends Component {
           ><Img src="./rightimg.png" width="214" height="550"/>
         </Grid>
         </Grid>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <Header />
+      {/* <Content /> */}
+    </div>
+  );
+  }
+}
+class Header extends Component {
+  render(){
+    return (
+      <div className="container">
+        <div class="pallet" id="boxzone">
+        </div>
+        <div class="subpallet"> 
+        </div>
+      </div>
     );
   }
 }
 
-//<div id="susi-modal-left-image"><div class="ll u f"><img alt="" class="" src="https://miro.medium.com/max/214/1*MQH4A5bsyRz4AWh5V4IfvQ.png" width="214" height="550" role="presentation"/></div></div>
-//<div class="kk" id="susi-modal-right-image"><div class="ll u f"><img alt="" class="" src="https://miro.medium.com/max/214/1*lhbp8cxKdkDB-MgmwIPE5w.png" width="214" height="550" role="presentation"></div></div>        
 
-export default App;
