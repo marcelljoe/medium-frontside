d052fbccd184023e02ffb034b4ba416d24beb22c
marcelljoe <57036356+marcelljoe@users.noreply.github.com>
2019-12-08 22:48:44 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import {Button, Header, Icon, Modal} from 'semantic-ui-react';
 import './App.css';
 import Registration from './Registration';
-import Login from './Login';
 
 class App extends Component {
   render(){
   return (
     <div className="App">
       <Registration />
-      <Login />
     </div>
   );
   }
